c8c8166f51fa0a5b6102f63fd3d6512ef06db516
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-10-31 18:57:01 +0100

    adding features to parts

--- src/components/monitor.js
+++ src/components/monitor.js
@@ -7,7 +7,7 @@ const Monitor = (props) => {
                 This is the time of the values: {props.date}
             </p>
             <div>
-            { props.parts ? props.parts.map(part => partsRender(part)) : ''}
+                {props.parts ? props.parts.map(part => partsRender(part)) : ''}
             </div>
         </div>
     );
@@ -15,8 +15,23 @@ const Monitor = (props) => {
 
 const partsRender = part => {
     return (
-        <div className="card-deck">
-           Part: {part.name}
+        <div>
+            <p className="lead">Part: {part.name}</p>
+            <div className="card-columns">
+                {part.features.map(feat => featuresRender(feat))}
+            </div>
+        </div>
+    );
+}
+
+const featuresRender = feat => {
+    return (
+        <div className="card">
+            <div className="card-inverse card-primary">
+                <p className="card-title text-white">
+                    {feat.name}
+                </p>
+            </div>
         </div>
     );
 }
